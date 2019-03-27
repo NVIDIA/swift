3b556f0cf2b2f914bc68ab2b5709ecc8b760d7c3
Tim Burke <tim.burke@gmail.com>
2019-03-27 15:28:50 -0700

    WIP: s3api: Make multi-deletes async
    Previously, S3 multi-deletes acted much like Swift bulk deletes; the
    proxy would spin up a few greenthreads to go issue DELETEs for each
    named object. This has the advantage of freeing up disk space
    immediately, but (1) relies on the full object-update path to clean up
    container listings and (2) takes a while to complete, as we have to
    aggregate all of these individual DELETEs.
    
    The listing-update lag in particular can cause cascading problems: some
    clients, wishing to delete all items with a given prefix, may issue
    repeated queries for that prefix to feed into a multi-delete request,
    without including a marker or continuation token from the previous
    listing. As a result, many DELETEs are issued for the same names,
    further overloading the container server that's trying to eat updates
    and causing listings to lag even further behind.
    
    Now, multi-deletes update listings immediately, and enqueue jobs to
    issue per-object deletes later. This has basically exactly the opposite
    set of trade-offs: listings update quickly, client requests are fast,
    but disk space takes a while to clean up and a GET for one of the
    deleted objects may still return data.
    
    Operators must specifically opt-in with the use_async_delete config
    option.  The option is incompatible with multipart uploads, acls and
    object versioning.
    
    This builds on interfaces introduced with the operator async-delete tool.
    
    Change-Id: I50e403dee75585fc1ff2bb385d6b2d2f13653cf8

--- swift/common/middleware/s3api/s3api.py
+++ swift/common/middleware/s3api/s3api.py
@@ -277,11 +277,22 @@ class S3ApiMiddleware(object):
             conf.get('min_segment_size', 5242880))
         self.conf.annotate_with_upload_id = config_true_value(
             conf.get('annotate_with_upload_id', True))
+        self.conf.use_async_delete = config_true_value(
+            conf.get('use_async_delete', False))
 
         self.logger = get_logger(
             conf, log_route=conf.get('log_name', 's3api'))
         self.slo_enabled = self.conf.allow_multipart_uploads
         self.check_pipeline(self.conf)
+
+        if self.conf.use_async_delete and (self.conf.s3_acl or
+                                           self.conf.allow_multipart_uploads):
+            self.conf.use_async_delete = False
+            self.logger.warning(
+                'use_async_delete is not compatible with s3_acl or '
+                'allow_multipart_uploads; it has been disabled')
+            # Alternatively, have the proxy refuse to start.
+            # We default the async to false; operator opted into it...
 
     def __call__(self, env, start_response):
         try:
--- swift/common/middleware/s3api/s3request.py
+++ swift/common/middleware/s3api/s3request.py
@@ -1189,6 +1189,9 @@ class S3Request(swob.Request):
                 'DELETE': [
                     HTTP_NO_CONTENT,
                 ],
+                'UPDATE': [  # used for bulk deletes
+                    HTTP_ACCEPTED,
+                ],
             }
         else:
             # Swift object access.
@@ -1262,6 +1265,8 @@ class S3Request(swob.Request):
                 'DELETE': {
                     HTTP_NOT_FOUND: (NoSuchBucket, container),
                     HTTP_CONFLICT: BucketNotEmpty,
+                },
+                'UPDATE': {
                 },
             }
         else:
