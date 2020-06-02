5690ddf75c8277345ac17728af8a8451850fc84f
Tim Burke <tim.burke@gmail.com>
2020-06-02 16:14:46 -0700

    Add a new URL parameter to allow for async cleanup of SLO segments
    Add a new config option to SLO, allow_async_delete, to allow operators
    to opt-in to this new behavior. If their expirer queues get out of hand,
    they can always turn it back off.
    
    If the option is disabled, handle the delete inline; this matches the
    behavior of old Swift.
    
    Only allow an async delete if all segments are in the same container and
    none are nested SLOs, that way we only have two auth checks to make.
    
    Have s3api try to use this new mode if the data seems to have been
    uploaded via S3 (since it should be safe to assume that the above
    criteria are met).
    
    Drive-by: Allow the expirer queue and swift-container-deleter to use
    high-precision timestamps.
    
    Change-Id: I0bbe1ccd06776ef3e23438b40d8fb9a7c2de8921

--- swift/common/middleware/s3api/s3request.py
+++ swift/common/middleware/s3api/s3request.py
@@ -1486,7 +1486,14 @@ class S3Request(swob.Request):
         if version is not None:
             query['version-id'] = version
         resp = self.get_response(app, 'HEAD', obj=obj, query=query)
-        return {'multipart-manifest': 'delete'} if resp.is_slo else {}
+        if not resp.is_slo:
+            return {}
+        elif resp.sysmeta_headers.get(sysmeta_header('object', 'etag')):
+            # Even if allow_asynd_delete is turned off, SLO will just handle
+            # the delete synchronously
+            return {'multipart-manifest': 'delete', 'async': 'on'}
+        else:
+            return {'multipart-manifest': 'delete'}
 
     def set_acl_handler(self, handler):
         pass
