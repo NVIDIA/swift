7d77e0ff4ea3c391a11ef7538fe11704fa02189d
Alistair Coles <alistairncoles@gmail.com>
2022-03-10 11:35:18 +0000

    s3api: add API support for optional bucket inventory
    If the swift installation supports bucket inventory then this feature
    can be enabled in the s3API by setting the new config option
    's3_inventory_enabled'.  By default this option is False.
    
    The feature can be restricted to a subset of buckets using the
    's3_inventory_allowed_paths' option.
    
    Change-Id: I7f41c337e481806ca42274810518b12b00304e17

--- test/unit/common/middleware/s3api/__init__.py
+++ test/unit/common/middleware/s3api/__init__.py
@@ -90,9 +90,13 @@ class S3ApiTestCase(unittest.TestCase):
             'force_swift_request_proxy_log': False,
             'allow_multipart_uploads': True,
             'min_segment_size': 5242880,
-            'log_level': 'debug'
+            'log_level': 'debug',
+            's3_inventory_enabled': False,
+            's3_inventory_allowed_paths': '*',
         }
+        self.make_app()
 
+    def make_app(self):
         self.app = FakeApp()
         self.swift = self.app.swift
         # note: self.conf has no __file__ key so check_pipeline will be skipped
