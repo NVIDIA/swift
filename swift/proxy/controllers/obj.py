7d60b48c8459520482f66a00f719be6b0d7eae08
indianwhocodes <nairashwin952013@gmail.com>
2023-02-21 13:26:06 -0800

    Add x-open-expired to recover expired objects
    When a client sets a 'x-open-expired' header to a true value for a
    GET/HEAD/POST request the proxy will forward x-backend-open-expired to
    storage server. The storage server will allow clients that set
    x-backend-open-expired to open and read an object even after the
    x-delete-at time as passed.
    
    During the period after an object is expired, but before it has been
    reaped by the object-expirer this new feature will be helpful for
    recovery of encrypted or EC data.
    
    Change-Id: Ibe7dde0e3bf587d77e14808b169c02f8fb3dddb3

--- swift/proxy/controllers/obj.py
+++ swift/proxy/controllers/obj.py
@@ -251,6 +251,8 @@ class BaseObjectController(Controller):
         policy = POLICIES.get_by_index(policy_index)
         obj_ring = self.app.get_object_ring(policy_index)
         req.headers['X-Backend-Storage-Policy-Index'] = policy_index
+        if config_true_value(req.headers.get('x-open-expired')):
+            req.headers['X-Backend-Open-Expired'] = 'true'
         if 'swift.authorize' in req.environ:
             aresp = req.environ['swift.authorize'](req)
             if aresp:
@@ -437,6 +439,8 @@ class BaseObjectController(Controller):
         container_partition, container_nodes, container_path = \
             self._get_update_target(req, container_info)
         req.acl = container_info['write_acl']
+        if config_true_value(req.headers.get('x-open-expired')):
+            req.headers['X-Backend-Open-Expired'] = 'true'
         if 'swift.authorize' in req.environ:
             aresp = req.environ['swift.authorize'](req)
             if aresp:
