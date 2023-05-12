10561ac89ed5f42c8cf026145de7e7563ef3ca27
Jianjian Huo <jhuo@nvidia.com>
2023-05-12 15:37:20 -0700

    proxy: print logs when write new shard range cache into memcache.
    Change-Id: I0e13e0148a6e19fd6d0a9438604aa791f948502a

--- swift/proxy/controllers/container.py
+++ swift/proxy/controllers/container.py
@@ -239,8 +239,8 @@ class ContainerController(Controller):
             memcache = cache_from_env(req.environ, True)
             if memcache and ns_bound_list:
                 # cache in memcache only if shard ranges as expected
-                self.logger.debug('Caching %d shards for %s',
-                                  len(ns_bound_list.bounds), req.path_qs)
+                self.logger.info('Caching %d listing shards for %s',
+                                 len(ns_bound_list.bounds), cache_key)
                 memcache.set(cache_key, ns_bound_list.bounds,
                              time=self.app.recheck_listing_shard_ranges)
         return ns_bound_list
