01cfeccd85fc25b653eb0caf13f82e6b3beb5f1e
Yan Xiao <yanxiao@nvidia.com>
2024-02-29 09:59:37 -0500

    Add labeled metrics to proxy-logging
    Modified code to use native labeled metrics API based on Tim Burke's
    patch for proxy-logging. Thanks Tim.
    
    Co-Authored-By: Tim Burke <tim.burke@gmail.com>
    Co-Authored-By: Alistair Coles <alistairncoles@gmail.com>
    
    Related-Change: I115ffb1dc601652a979895d7944e011b951a91c1
    
    Change-Id: I5a96626862644fe23a0d4bc53e85989770c8ceec

--- test/unit/common/utils/test_logs.py
+++ test/unit/common/utils/test_logs.py
@@ -933,6 +933,10 @@ class TestUtilsLogs(unittest.TestCase):
         self.assertRaises(ValueError, utils.StrAnonymizer,
                           'Swift is great!', 'sha257', '')
 
+        anon = utils.StrAnonymizer(None, 'sha1', 'salty_secret')
+        self.assertEqual(anon, '')
+        self.assertEqual(anon.anonymized, '')
+
     def test_str_anonymizer_python_maddness(self):
         with mock.patch('swift.common.utils.base.hashlib') as mocklib:
             if six.PY2:
