640902ee88380091410a08f5669e1383e5a9bb4b
Clay Gerrard <clay.gerrard@gmail.com>
2024-03-28 15:06:51 -0500

    DNM expirer: new options to control task iteration
    We're carrying this patch and actively working on extending the design
    in follow-up changes.  It is not currently appropriate to merge because
    it contains backwards compatibility shims that are only relevant to
    previous un-released versions of this patch which will be removed.
    
    Particularly when using delay_reaping processing expirer task containers
    in order is likely to impose a significant delay after restart before
    any object-expirers begin to process DELETE requests.  In addition it's
    useful to distribute the expirer container listing load randomly between
    all object nodes processing expirer task containers - since they all
    need to list all "due" task containers, but don't necessarily need to
    process them in the same order.
    
    With many expirer processes configured it can take many task_containers
    worth of tasks to fill the default MAX_OBJECTS_TO_CACHE and start
    processing DELETEs.  This effect is more pronounced with randomized task
    queue iteration in that even if you get lucky and iterate a ready
    container sooner you may not have enough objects to fill your cache and
    start doing DELETEs.
    
    Change-Id: I8879dbd13527233c878dff764ec411ce9619ee39

--- swift/obj/expirer.py
+++ swift/obj/expirer.py
@@ -16,7 +16,7 @@
 import six
 from six.moves import urllib
 
-from random import random
+from random import random, shuffle
 from time import time
 from os.path import join
 from collections import defaultdict, deque
@@ -194,6 +194,31 @@ class ObjectExpirer(Daemon):
 
         self.delay_reaping_times = read_conf_for_delay_reaping_times(conf)
 
+        valid_task_container_iteration_strategies = {'legacy', 'randomized'}
+        self.task_container_iteration_strategy = conf.get(
+            'task_container_iteration_strategy', 'legacy')
+        # XXX temporary shim to support the un-released configuration option
+        if config_true_value(conf.get(
+                'randomized_task_container_iteration', 'false')):
+            self.task_container_iteration_strategy = "randomized"
+        # randomized task container iteration can be useful if there's lots of
+        # tasks in the queue under a configured delay_reaping
+        if self.task_container_iteration_strategy not in \
+                valid_task_container_iteration_strategies:
+            self.logger.warning(
+                "Unrecognized config value: "
+                "'task_container_iteration_strategy = %s' "
+                "(using legacy)",
+                self.task_container_iteration_strategy)
+            self.task_container_iteration_strategy = "legacy"
+
+        # with lots of nodes and lots of tasks a large cache size can
+        # significantly delay processing; and caching tasks to round_robin
+        # target container order may be less necessary if there's only a few
+        # target containers or with randomized task container iteration
+        self.round_robin_task_cache_size = int(
+            conf.get('round_robin_task_cache_size', MAX_OBJECTS_TO_CACHE))
+
     def read_conf_for_queue_access(self, swift):
         self.expiring_objects_account = AUTO_CREATE_ACCOUNT_PREFIX + \
             (self.conf.get('expiring_objects_account_name') or
@@ -271,7 +296,7 @@ class ObjectExpirer(Daemon):
             obj_cache[cache_key].append(delete_task)
             cnt += 1
 
-            if cnt > MAX_OBJECTS_TO_CACHE:
+            if cnt > self.round_robin_task_cache_size:
                 for task in dump_obj_cache_in_round_robin():
                     yield task
                 cnt = 0
@@ -314,30 +339,35 @@ class ObjectExpirer(Daemon):
         # task_container name is timestamp
         return Timestamp(task_container)
 
-    def iter_task_containers_to_expire(self, task_account):
+    def iter_task_account_containers_to_expire(self, task_account):
         """
-        Yields task_container names under the task_account if the delete at
-        timestamp of task_container is past.
+        Yields (task_account, task_container) tuples under the task_account if
+        the delete at timestamp of task_container is past.
         """
+        container_list = []
         for c in self.swift.iter_containers(task_account,
                                             prefix=self.task_container_prefix):
             task_container = str(c['name'])
             timestamp = self.delete_at_time_of_task_container(task_container)
             if timestamp > Timestamp.now():
                 break
-            yield task_container
+            container_list.append(task_container)
+        if self.task_container_iteration_strategy == 'randomized':
+            shuffle(container_list)
+        for task_container in container_list:
+            yield task_account, task_container
 
     def get_delay_reaping(self, target_account, target_container):
         return get_delay_reaping(self.delay_reaping_times, target_account,
                                  target_container)
 
-    def iter_task_to_expire(self, task_account_container_list,
+    def iter_task_to_expire(self, task_account_container_iter,
                             my_index, divisor):
         """
         Yields task expire info dict which consists of task_account,
         task_container, task_object, timestamp_to_delete, and target_path
         """
-        for task_account, task_container in task_account_container_list:
+        for task_account, task_container in task_account_container_iter:
             container_empty = True
             for o in self.swift.iter_objects(task_account, task_container):
                 container_empty = False
@@ -433,9 +463,12 @@ class ObjectExpirer(Daemon):
                         'container_count': container_count,
                         'obj_count': obj_count})
 
-                task_account_container_list = \
-                    [(task_account, task_container) for task_container in
-                     self.iter_task_containers_to_expire(task_account)]
+                # For backwards compatibility with tests, the
+                # task_account_container_iter is passed into
+                # iter_task_to_expire rather than constructed within
+                # iter_task_to_expire.
+                task_account_container_iter = \
+                    self.iter_task_account_containers_to_expire(task_account)
 
                 # delete_task_iter is a generator to yield a dict of
                 # task_account, task_container, task_object, delete_timestamp,
@@ -443,7 +476,7 @@ class ObjectExpirer(Daemon):
                 # from the queue.
                 delete_task_iter = \
                     self.round_robin_order(self.iter_task_to_expire(
-                        task_account_container_list, my_index, divisor))
+                        task_account_container_iter, my_index, divisor))
                 rate_limited_iter = RateLimitedIterator(
                     delete_task_iter,
                     elements_per_second=self.tasks_per_second)
