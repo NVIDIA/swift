7963f940b5ea442a75685f58df510ecb10b8da71
Clay Gerrard <clay.gerrard@gmail.com>
2024-08-27 16:20:08 -0500

    object-server: raise 500 on ec timestamp collision
    Basically if trying to create a new non-durable EC object at the exact
    same timestamp while another thread is trying to commit: just blow up
    the second writer so it's never even non-durable and let the first one
    try to finish.  If neither proxy request gets a durable quorum they both
    return a 503 response. Well behaved clients should try and re-upload a
    PUT that returns 503.
    
    In order to only raise 500 for concurrent PUT collision with same
    timestamp, object-server also use metadata-aware collision detection:
    if metadata matches (in case of collision with 3R replication under the
    same PUT request), PUT will silently succeed; only if metadata differs,
    raise FileExistsError exception and 500 Error back to proxy server.
    
    NOTE: The object-server traceback is not very subtle.
    
    This patch is a MINOR improvement in that we won't return a 201 to a
    client unless a durable quorum of consistent frag set is written.
    
    ... not a complete set mind you, just a measly quorum; the rest can be
    inconsistent; so this patch is only *barely* useful.
    
    There is ongoing work to ensure that a quorum of frags are guaranteed to
    be or become consistent.  Even with this change, it is possible for a
    non-durable frag to be written while there are other durables with the
    same timestamp.  In this case we see that ssync will unfortunately make
    the non-conforming frags durable leaving you basically in the same state
    as you were on master today (excpet the proxy is guaranteed to be able
    to read from the consistent frag set; at least until you loose disks).
    This change doens't include that test only because we don't know how to
    make it stable yet [1].
    
    [1] Related-Change-Id: I0e52517927815a4731c37adb3d7c85d82e60a3c9
    
    Partial-Bug: #1971686
    
    Change-Id: Ib4690e274e3d05d4da3ad1322e1809b56f60288b
    Co-Authored-By: Jianjian Huo <jhuo@nvidia.com>
    Co-Authored-By: Alistair Coles <alistairncoles@gmail.com>
    Signed-off-by: Clay Gerrard <clay.gerrard@gmail.com>

--- test/unit/common/test_utils.py
+++ test/unit/common/test_utils.py
@@ -2181,7 +2181,7 @@ cluster_dfw1 = http://dfw1.host/v1/
     @requires_o_tmpfile_support_in_tmp
     @with_tempdir
     def test_link_fd_to_path_target_exists(self, tempdir):
-        # Create and write to a file
+        # unlike os.rename linkat will not allow timestamp collision
         fd, path = tempfile.mkstemp(dir=tempdir)
         os.write(fd, b"hello world")
         os.fsync(fd)
@@ -2192,10 +2192,12 @@ cluster_dfw1 = http://dfw1.host/v1/
         try:
             os.write(fd, b"bye world")
             os.fsync(fd)
-            utils.link_fd_to_path(fd, path, 0, fsync=False)
-            # Original file now should have been over-written
+            with self.assertRaises(OSError) as ctx:
+                utils.link_fd_to_path(fd, path, 0, fsync=False)
+            self.assertEqual(ctx.exception.errno, errno.EEXIST)
+            # Original file should NOT have been over-written
             with open(path, 'rb') as f:
-                self.assertEqual(f.read(), b"bye world")
+                self.assertEqual(f.read(), b"hello world")
         finally:
             os.close(fd)
 
@@ -2209,7 +2211,7 @@ cluster_dfw1 = http://dfw1.host/v1/
                 self.assertEqual(err.errno, errno.EACCES)
             else:
                 self.fail("Expecting IOError exception")
-        self.assertTrue(_m_linkat.called)
+        self.assertEqual(1, len(_m_linkat.mock_calls))
 
     def test_link_fd_to_path_runs_out_of_retries(self):
         _m_linkat = mock.Mock(
