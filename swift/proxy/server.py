ece5332714e56851be137d1926b97aa00296cbc5
Clay Gerrard <clay.gerrard@gmail.com>
2024-10-16 09:23:12 -0500

    Deprecate expirer options
    The following configuration options are deprecated:
    
     * expiring_objects_container_divisor
     * expiring_objects_account_name
    
    The upstream maintainers are not aware of any clusters where these have
    been configured to non-default values.
    
    UpgradeImpact:
    
    Operators are encouraged to remove their "container_divisor" setting and
    use the default value of 86400.
    
    If a cluster was deployed with a non-standard "account_name" operators
    should remove the option from all configs so they are using a supported
    configuration going forward, but will need to deploy a stand-alone
    legacy expirer config to clean-up old expiration tasks from the
    previously configured account name.
    
    Co-Authored-By: Alistair Coles <alistairncoles@gmail.com>
    Co-Authored-By: Jianjian Huo <jhuo@nvidia.com>
    Change-Id: I5ea9e6dc8b44c8c5f55837debe24dd76be7d6248

--- swift/proxy/server.py
+++ swift/proxy/server.py
@@ -51,6 +51,7 @@ from swift.common.swob import HTTPBadRequest, HTTPForbidden, \
     wsgi_to_str
 from swift.common.exceptions import APIVersionError
 from swift.common.wsgi import run_wsgi
+from swift.obj import expirer
 
 DEFAULT_NAMESPACE_CACHE_USE_TOKEN = False
 DEFAULT_NAMESPACE_CACHE_TOKEN_RETRY_INTERVAL = 0.1  # seconds
@@ -277,10 +278,8 @@ class Application(object):
             config_true_value(conf.get('account_autocreate', 'no'))
         self.auto_create_account_prefix = \
             constraints.AUTO_CREATE_ACCOUNT_PREFIX
-        self.expiring_objects_account = self.auto_create_account_prefix + \
-            (conf.get('expiring_objects_account_name') or 'expiring_objects')
-        self.expiring_objects_container_divisor = \
-            int(conf.get('expiring_objects_container_divisor') or 86400)
+        self.expirer_config = expirer.ExpirerConfig(
+            conf, container_ring=self.container_ring, logger=self.logger)
         self.max_containers_per_account = \
             int(conf.get('max_containers_per_account') or 0)
         self.max_containers_whitelist = [
